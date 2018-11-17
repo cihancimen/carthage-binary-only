8e4815a0c5b669d445a8f35e2cb5591765b06d1d
Cihan Cimen <CCimen@walmartlabs.com>
2018-11-16 17:03:01 -0800

    Dummy project

---
+++ GoogleMobileAds/GoogleMobileAds/GoogleMobileAds.h
@@ -0,0 +1,19 @@
+//
+//  GoogleMobileAds.h
+//  GoogleMobileAds
+//
+//  Created by Cihan Cimen on 11/16/18.
+//  Copyright © 2018 Walmartlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleMobileAds.
+FOUNDATION_EXPORT double GoogleMobileAdsVersionNumber;
+
+//! Project version string for GoogleMobileAds.
+FOUNDATION_EXPORT const unsigned char GoogleMobileAdsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMobileAds/PublicHeader.h>
+
+
